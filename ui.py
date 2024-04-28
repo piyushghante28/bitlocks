96f9555341934fc1caa55a71c30f15690cb7ff43
piyushghante28 <120301521+piyushghante28@users.noreply.github.com>
2024-04-29 02:02:53 +0530

    Update ui.py

--- ui.py
+++ ui.py
@@ -6,8 +6,8 @@ def main():
     st.markdown("### Quick Links:")
     if st.button("Encrypt Files with BitLocks"):
         st.write("[Encrypt Files with BitLocks](https://bitlocks-encryption.streamlit.app/)")
-    if st.button("[Decrypt Files with BitLocks](https://bitlocks-decryption.streamlit.app/)"):
-        st.write("")
+    if st.button("Decrypt Files with BitLocks"):
+        st.write("[Decrypt Files with BitLocks](https://bitlocks-decryption.streamlit.app/)")
 
 if __name__ == "__main__":
     main()
